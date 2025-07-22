cac59915ef9f6a4626db4c1c5f4ccd0ca1594b58
paulsamuel-w-e <work.paulsamuel@gmail.com>
2025-07-22 21:00:15 +0530

    added executables and scripts

--- scripts/run_and_stop.ps1
+++ scripts/run_and_stop.ps1
@@ -3,13 +3,13 @@ $workDir = (Get-Location).Path
 # === Launch FastAPI in a new terminal ===
 $fastapiProc = Start-Process powershell -PassThru -ArgumentList @(
     "-NoExit", 
-    "-Command", ". '$workDir/env/hvenv/Scripts/Activate.ps1'; uvicorn main_fastapi:app --reload --port 8000"
+    "-Command", ". '$workDir\..\env\hvenv\Scripts\Activate.ps1'; uvicorn ..\main_fastapi:app --reload --port 8000"
 ) -WorkingDirectory $workDir -WindowStyle Normal
 
 # === Launch Streamlit in a new terminal ===
 $streamlitProc = Start-Process powershell -PassThru -ArgumentList @(
     "-NoExit", 
-    "-Command", ". '$workDir/env/hvenv/Scripts/Activate.ps1'; streamlit run streamlit_ui/app.py"
+    "-Command", ". '$workDir\..\env\hvenv\Scripts\Activate.ps1'; streamlit run ..\streamlit_ui\app.py"
 ) -WorkingDirectory $workDir -WindowStyle Normal
 
 # === Prompt to stop servers ===
